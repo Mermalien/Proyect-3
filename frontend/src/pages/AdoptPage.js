8a36ab6a714e8db79190c0f77d50f38cf172abed
PatriciaPec <bruj341@gmail.com>
2023-02-25 00:01:22 +0100

    cambios adoptpage,animalsplantspage y css de aoptpage y animalsplantspage

--- frontend/src/pages/AdoptPage.js
+++ frontend/src/pages/AdoptPage.js
@@ -1,21 +1,43 @@
 import { Link } from "react-router-dom";
+import "../styles/AdoptPage.css";
 
 export const AdoptPage = () => {
   return (
     <div>
-    <section>
-      <Link to="/adopta">
-        <h1>¿Estás pensando en adoptar?</h1>
-      </Link>
-    </section>
+      <section>
+        <Link to="/adopta">
+          <h1>¿Estás pensando en adoptar?</h1>
+        </Link>
+        <Link to={"/"}>
+          <p>Volver al inicio</p>
+        </Link>
+      </section>
 
-<ul>
-<li>
-    <a href= "https://www.addaong.org/es/informacion/listado-de-protectoras-de-animales/" className='protectoras' target="_blank" rel="noreferrer">Protectoras de animales</a>
-    <a href="https://www.protectoras.es/" className='otrasprotectoras' target="_blank" rel="noreferrer">Otras protectoras</a>
-</li>
-</ul>
-</div>
-
+      <ul>
+        <li>
+          <a
+            href="https://www.addaong.org/es/informacion/listado-de-protectoras-de-animales/"
+            className="protectoras"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Protectoras de animales
+          </a>
+          <a
+            href="https://www.protectoras.es/"
+            className="otrasprotectoras"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Otras protectoras
+          </a>
+        </li>
+      </ul>
+      <img
+        src="/images/adopcion1.jpg"
+        alt="Foto de letras adopt"
+        style={{ height: "300px", width: "300px" }}
+      />
+    </div>
   );
 };
--- frontend/src/pages/AnimalsPlantsPage.js
+++ frontend/src/pages/AnimalsPlantsPage.js
@@ -1,17 +1,65 @@
 import { Link } from "react-router-dom";
-
+import "../styles/AnimalsPlantsPage.css";
 
 export const AnimalsPlantsPage = () => {
   return (
     <main>
-
       <Link to="/plants">
         <h1>Tips para animales y plantas en la misma casa</h1>
       </Link>
-
-
-      <Link to={'/'}><p>Volver al inicio</p></Link>
+      <Link to={"/"}>
+        <p>Volver al inicio</p>
+      </Link>
+      <ul>
+        <li>
+          <a
+            href="https://centroveterinarioneptuno.es/noticias/plantas-toxicas-para-perros-y-gatos-conoce-sus-peligros/"
+            className="perrosygatos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Plantas tóxicas para perros y gatos
+          </a>
+          <a
+            href="https://www.leonvet.es/noticias-animales/plantas-toxicas-para-perros.html"
+            className="perros"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Plantas tóxicas para perros
+          </a>
+          <a
+            href="https://ecoinventos.com/plantas-toxicas-mascotas/"
+            className="mascotas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Plantas tóxicas para mascotas
+          </a>
+          <a
+            href="https://animalesexoticos24h.es/plantas-peligrosas-conejos/"
+            className="conejos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Plantas tóxicas para conejos
+          </a>
+          <a
+            href="https://www.mundoanimalia.com/animales-consejos/plantas-toxicas-para-nuestras-mascotas.html"
+            params="quehacer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Plantas tóxicas para mascotas y que hacer si se intoxica
+          </a>
+        </li>
+      </ul>
+      <img
+        src="/images/aloevera.jpg"
+        alt="¨Foto de aloevera"
+        style={{ height: "200px", width: "200px" }}
+      />
     </main>
-    
   );
 };
