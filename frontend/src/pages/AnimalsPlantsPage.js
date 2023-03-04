f0e6031bd81ded004c3edec61ffaee53db7328b0
PatriciaPec <bruj341@gmail.com>
2023-03-04 21:35:29 +0100

    cambios plantas

--- frontend/src/pages/AnimalsPlantsPage.js
+++ frontend/src/pages/AnimalsPlantsPage.js
@@ -3,14 +3,14 @@ import "../styles/AnimalsPlantsPage.css";
 
 export const AnimalsPlantsPage = () => {
   return (
-    <main>
+    <main className="mainplants">
       <Link to="/plants">
-        <h1>Tips para animales y plantas en la misma casa</h1>
+        <h1>Muestra de alimentos y plantas tóxicos para las mascotas </h1>
       </Link>
       <Link to={"/"}>
         <p>Volver al inicio</p>
       </Link>
-      <ul>
+      <ul className="ulplants">
         <li>
           <a
             href="https://centroveterinarioneptuno.es/noticias/plantas-toxicas-para-perros-y-gatos-conoce-sus-peligros/"
@@ -47,15 +47,48 @@ export const AnimalsPlantsPage = () => {
           </a>
           <a
             href="https://www.mundoanimalia.com/animales-consejos/plantas-toxicas-para-nuestras-mascotas.html"
-            params="quehacer"
+            className="quehacer"
             target="_blank"
             rel="noreferrer"
           >
             Plantas tóxicas para mascotas y que hacer si se intoxica
           </a>
+          <a
+            href="https://soyunperro.com/alimentos-toxicos-para-perros/"
+            className="alimentostoxicosperros"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alimentos tóxicos para perros
+          </a>
+          <a
+            href="https://www.royalcanin.com/es/cats/health-and-wellbeing/what-foods-are-toxic-for-my-cat"
+            className="alimentostoxicosgatos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alimentos y plantas tóxicas para gatos
+          </a>
+          <a
+            href="https://www.petdarling.com/alimentos-toxicos-para-gatos/"
+            className="alimentostoxicosgatos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alimentos tóxicos para gatos
+          </a>
+          <a
+            href="https://www.expertoanimal.com/alimentos-prohibidos-para-conejos-21771.html"
+            className="alimentostoxicosconejos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alimentos tóxicos para conejos
+          </a>
         </li>
       </ul>
       <img
+        className="imgplants"
         src="/images/aloevera.jpg"
         alt="¨Foto de aloevera"
         style={{ height: "200px", width: "200px" }}
